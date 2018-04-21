3786c1f3a0739f915a8975614e1bc78ee574b0d7
OHTA Shogo <athos0220@gmail.com>
2018-04-21 17:33:04 +0900

    Implement intent handlers for playing Podcasts

--- src/service/kodamacast/handlers.cljs
+++ src/service/kodamacast/handlers.cljs
@@ -18,3 +18,23 @@
 
 (defhandler :AMAZON.StopIntent
   (h/emit ":tell" (h/t "THANKS_MESSAGE")))
+
+(def PODCAST_URL "https://s3.amazonaws.com/cognicast/shows/cognicast-103-rich-hickey-spec.mp3")
+
+(defhandler :Play
+  (.. &self
+      -response
+      (audioPlayerPlay "REPLACE_ALL" PODCAST_URL PODCAST_URL nil 0))
+  (h/emit ":responseReady"))
+
+(defhandler :AMAZON.PauseIntent
+  (.. &self
+      -response
+      (audioPlayerStop))
+  (h/emit ":responseReady"))
+
+(defhandler :AMAZON.ResumeIntent
+  (.. &self
+      -response
+      (audioPlayerPlay "REPLACE_ALL" PODCAST_URL PODCAST_URL nil 0))
+  (h/emit ":responseReady"))
