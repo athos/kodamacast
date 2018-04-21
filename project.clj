7b5e5ab63b94d5796907b7a8ff52a49d692be33f
OHTA Shogo <athos0220@gmail.com>
2018-04-21 16:01:01 +0900

    Setup CLJS

--- project.clj
+++ project.clj
@@ -1,6 +1,20 @@
 (defproject kodamacast "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/athos/kodamacast"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.10.238"]]
+
+  :plugins [[lein-cljsbuild "1.1.7"]]
+
+  :cljsbuild {:builds
+              [{:id "service"
+                :source-paths ["src/service"]
+                :compiler {:output-to "target/service/kodamacast.js"
+                           :output-dir "target/service"
+                           :main kodamacast.main
+                           :optimizations :simple
+                           :target :nodejs
+                           :npm-deps {"alexa-sdk" "^1.0.25"}
+                           :install-deps true}}]})
