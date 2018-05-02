05761855b125c74252fbbd9f81d9e775175dd8d0
OHTA Shogo <athos0220@gmail.com>
2018-05-02 20:20:54 +0900

    Reimplement request handlers using new handler core

--- service/src/kodamacast/handlers.cljs
+++ service/src/kodamacast/handlers.cljs
@@ -1,47 +1,56 @@
 (ns kodamacast.handlers
   (:require [kodamacast.handlers.core :as h :refer [defhandler]]))
 
-(defhandler :LaunchRequest
-  (h/emit ":ask" (h/t "DESCRIPTION") ""))
+(defhandler LaunchRequest
+  ^{:target (fn [input]
+              (js/console.log "input:" input)
+              (= (h/request-type input) "LaunchRequest"))}
+  [input]
+  (.. (.-responseBuilder input)
+      (speak (h/t "DESCRIPTION"))
+      (getResponse)))
 
-(defhandler :FinishIntent
-  (h/emit "AMAZON.StopIntent"))
+(defhandler FinishIntent
+  ^{:target #(and (h/intent-request? %)
+                  (contains? #{"AMAZON.CancelIntent" "AMAZON.StopIntent"}
+                             (h/intent-name %)))}
+  [input]
+  (.. (.-responseBuilder input)
+      (speak (h/t "THANKS_MESSAGE"))
+      (getResponse)))
 
-(defhandler :Unhandled
-  (h/emit ":ask" (h/t "SORRY_MESSAGE") (h/t "TRY_AGAIN_MESSAGE")))
+(defhandler Unhandled [input]
+  (.. (.-responseBuilder input)
+      (speak (h/t "SORRY_MESSAGE"))
+      (reprompt (h/t "TRY_AGAIN_MESSAGE"))
+      (getResponse)))
 
-(defhandler :AMAZON.HeplIntent
-  (h/emit ":ask" (h/t "HELP_MESSAGE") ""))
-
-(defhandler :AMAZON.CancelIntent
-  (h/emit "AMAZON.StopIntent"))
-
-(defhandler :AMAZON.StopIntent
-  (h/emit ":tell" (h/t "THANKS_MESSAGE")))
+(defhandler AMAZON.HeplIntent [input]
+  (.. (.-responseBuilder input)
+      (speak (h/t "HELP_MESSAGE"))
+      (getResponse)))
 
 (def PODCAST_URLS
   {"こぐにきゃすと" "https://s3.amazonaws.com/cognicast/shows/cognicast-103-rich-hickey-spec.mp3"
    "やっていきえふえむ" "https://yatteiki.fm/audio/53.mp3"})
 
-(defhandler :PlayPodcast
-  (let [slots (.. &self -event -request -intent -slots)
+(defhandler PlayPodcast [input]
+  (let [slots (.. input -requestEnvelope -request -intent -slots)
         podcast-name (-> slots (aget "podcastName") .-value)]
     (if-let [podcast-url (get PODCAST_URLS podcast-name)]
-      (do (doto (.-response &self)
-            (.audioPlayerPlay "REPLACE_ALL" podcast-url podcast-url nil 0)
-            (.speak (str podcast-name "を再生します。")))
-          (h/emit ":responseReady"))
-      (h/emit ":tell"
-              (str podcast-name "に該当するポッドキャストは見つかりませんでした。")))))
+      (.. (.-responseBuilder input)
+          (addAudioPlayerPlayDirective "REPLACE_ALL" podcast-url podcast-url nil 0)
+          (speak (str podcast-name "を再生します。"))
+          (getResponse))
+      (.. (.-responseBuilder input)
+          (speak (str podcast-name "に該当するポッドキャストは見つかりませんでした。"))
+          (getResponse)))))
 
-(defhandler :AMAZON.PauseIntent
-  (.. &self
-      -response
-      (audioPlayerStop))
-  (h/emit ":responseReady"))
+(defhandler AMAZON.PauseIntent [input]
+  (.. (.-responseBuilder input)
+      (addAudioPlayerStopDirective)
+      (getResponse)))
 
-(defhandler :AMAZON.ResumeIntent
-  #_(.. &self
-      -response
-      (audioPlayerPlay "REPLACE_ALL" PODCAST_URL PODCAST_URL nil 0))
-  (h/emit ":responseReady"))
+(defhandler AMAZON.ResumeIntent [input]
+  (.. (.-responseBuilder input)
+      (getResponse)))
